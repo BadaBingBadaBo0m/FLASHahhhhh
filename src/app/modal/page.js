184f707416bf1e5a027617126aa2a8937ed93bff
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-14 17:57:48 -0700

    added signin modal

--- src/app/modal/page.js
+++ src/app/modal/page.js
@@ -2,21 +2,27 @@
 
 import React, { useState } from "react";
 import SignupForm from "@/components/SignupModal";
+import SignInForm from "@/components/SignInModal";
 import { useModal } from "@/context/Modal";
 
 const Modals = () => {
   const { setModalContent } = useModal();
 
-  const openModal = () => {
+  const openSignup = () => {
     setModalContent(<SignupForm />)
   };
+
+  const openLogin = () => {
+    setModalContent(<SignInForm />)
+  }
 
   return (
     <>
       <div>
         <h1>bruh</h1>
 
-        <button onClick={openModal}>Open modal</button>
+        <button onClick={openSignup}>Open Signup</button>
+        <button onClick={openLogin}>Open Login</button>
       </div>
     </>
   )
---
+++ src/components/SignInModal/index.js
@@ -0,0 +1,63 @@
+"use client"
+
+import React, { useState } from 'react';
+import { auth } from '@/app/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log('user signed in', user)
+        router.push('/')
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('errors', errorMessage)
+      });
+
+    console.log("bruh");
+  }
+
+
+  return (
+    <div className='text-black'>
+      <form onSubmit={handleSubmit} className="flex flex-col w-96 h-96 p-8 items-center justify-between border-2 border-border-blue rounded-2xl bg-nav-background">
+        <h1 className='text-3xl'>Sign up</h1>
+
+        <input
+          placeholder="Email Address"
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className='w-64 h-11 p-2 rounded-sm outline-none bg-nav-background border border-gray-400'
+        />
+        <input
+          placeholder="Password"
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          name="new-password"
+          autoComplete='on'
+          className='w-64 h-11 p-2 rounded-sm outline-none bg-nav-background border border-gray-400'
+        />
+
+        <button className='bg-blue-500 w-36 h-11 rounded-full font-bold text-lg' type="submit" onClick={handleSubmit}>Sign up</button>
+        <div>Already have an account? <a className='cursor-pointer text-blue-500' onClick={() => router.push('/login')}>Login</a>!</div>
+      </form>
+    </div>
+  )
+}
+
+export default SignInForm;
