14a672785b48c767478afe4a2c9b2d11d17263a3
Brian <bw49078@gmail.com>
2024-08-15 13:50:56 -0500

    can now query flash card sets from firebase

--- src/app/flashcard/page.js
+++ src/app/flashcard/page.js
@@ -1,4 +1,26 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { collection, addDoc, query, onSnapshot, orderBy } from "firebase/firestore"; 
+import { db } from "../firebase";
+
 const FlashCardTest = () =>{
+  const [cardSets, setCardSets] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "FlashCard-Set"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const setArr = [];
+      querySnapshot.forEach((doc) => {
+        setArr.push({ ...doc.data(), id: doc.id });
+      });
+      
+      setCardSets(setArr);
+    });
+  }, []);
+
+  console.log(cardSets);
+
   return (
     <div className="w-[60%] m-auto pt-11">
       <h1> Hello</h1>
