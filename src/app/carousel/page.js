33e3584b84fdb9a5c43960457a68c0dfabf3e146
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-14 19:24:27 -0700

    merged navbar

--- src/app/carousel/page.js
+++ src/app/carousel/page.js
@@ -2,14 +2,14 @@ import Carousel from "@/components/Carousel";
 
 const CarouselTest = () => {
   let slides = [
-    {prompt: "Hi how are ya", answer: "Good how about you"},
-    {prompt: "You are a weird feller", answer: "That's what she said"},
-    {prompt: "What's the best gum ever?", answer: "Charleston chew"},
-    {prompt: "Bruh you suck", answer: "I know"},
+    { prompt: "Hi how are ya", answer: "Good how about you" },
+    { prompt: "You are a weird feller", answer: "That's what she said" },
+    { prompt: "What's the best gum ever?", answer: "Charleston chew" },
+    { prompt: "Bruh you suck", answer: "I know" },
   ];
 
   return (
-    <div className="w-[60%] h-full m-auto pt-11 ">
+    <div className="w-[60%] h-screen m-auto pt-11 ">
       <Carousel slides={slides} />
     </div>
   );
