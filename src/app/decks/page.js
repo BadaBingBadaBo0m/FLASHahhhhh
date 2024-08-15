ba92e87d66e53c1ea5fc69f92024f424764609d4
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-15 16:36:48 -0700

    fetches decks

--- src/app/decks/page.js
+++ src/app/decks/page.js
@@ -1,14 +1,29 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
+import { collection, addDoc, query, onSnapshot, orderBy, doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
 import { BsPlusCircleFill } from 'react-icons/bs';
 import { auth } from '../firebase';
 import { useRouter } from 'next/navigation';
 
 const FlashCardHome = () => {
-  const [deckList, setDeckList] = useState([]);
+  const [decks, setDecks] = useState([]);
   const router = useRouter();
   const currentUser = auth.currentUser;
+
+  useEffect(() => {
+    const q = query(collection(db, "Decks"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const deckArr = [];
+      querySnapshot.forEach((doc) => {
+        deckArr.push({ ...doc.data(), id: doc.id });
+      });
+
+      setDecks(deckArr);
+    });
+  }, []);
 
   return (
     <div>
@@ -27,9 +42,10 @@ const FlashCardHome = () => {
             <div>Create new deck</div>
           </div>
 
-          {deckList.map(deck => (
-            <div>
-              <h1>Deck</h1>
+          {decks.map(deck => (
+            <div className='flex gap-5'>
+              <h1>{deck.Name}</h1>
+              <button onClick={() => console.log(deck)}>Print</button>
             </div>
           ))}
         </div>
