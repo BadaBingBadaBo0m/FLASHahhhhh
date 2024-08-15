b5ff5fff701511aa00f6414d7887f9c0d60a3352
Brian <bw49078@gmail.com>
2024-08-14 21:28:07 -0500

    scroll bar gone

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen bg-background`}>
+      <body className={`${inter.className} min-h-screen bg-background overflow-y-hidden`}>
         <ModalProvider >
           <NavBar />
           {children}
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-full flex-col items-center justify-between p-24">
+    <div className="flex h-full flex-col items-center justify-between p-24 ">
       <h1>FLASH ahhhhhh</h1>
     </div>
   );
