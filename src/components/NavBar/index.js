f8bdf8c6452950807a2dfd3f630aeea9a91306a2
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-14 18:37:22 -0700

    moved login and signup buttons. added name to navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import SignInForm from '../SignInModal';
+import SignupForm from '../SignupModal';
 import { useModal } from '@/context/Modal';
 
 const NavBar = () => {
@@ -9,10 +10,11 @@ const NavBar = () => {
 
   return (
     <div className='flex justify-between p-5'>
-      <h1>FLASH - <span>ahhhhh</span></h1>
+      <h1 className='flex items-center'>FLASH - <span className='text-xs ml-1'>ahhhhh</span></h1>
 
       <div id='nav-button-container'>
-        <button className='bg-blue-500 w-20 h-10 rounded-full font-bold text-sm' onClick={() => setModalContent(<SignInForm />)}>Sign In</button>
+        <button className='bg-blue-500 w-20 h-10 rounded-l-lg font-bold text-sm mr-[3px]' onClick={() => setModalContent(<SignInForm />)}>Sign In</button>
+        <button className='bg-blue-500 w-20 h-10 rounded-r-lg font-bold text-sm' onClick={() => setModalContent(<SignupForm />)}>Sign Up</button>
       </div>
     </div>
   )
