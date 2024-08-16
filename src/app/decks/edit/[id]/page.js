96401328c8f85eecf44fee64d384c4b10cb267c9
Brian <bw49078@gmail.com>
2024-08-16 15:23:05 -0500

    added current deck to usestate

--- src/app/decks/edit/[id]/page.js
+++ src/app/decks/edit/[id]/page.js
@@ -10,11 +10,12 @@ import { useModal } from '@/context/Modal';
 export default function DeckPage() {
   const pathname = usePathname().split('/');
   const deckId = pathname[pathname.length - 1];
-  const currentSet = doc(db, "Decks", deckId)
+  const currentSet = doc(db, "Decks", deckId);
+  const [currentDeck, setCurrentDeck] = useState({});
 
   const getOneDoc = async () => {
     const currentSetSnap = await getDoc(currentSet);
-    console.log(currentSetSnap.data())
+    setCurrentDeck(currentSetSnap.data());
   }
 
   useEffect(() => {
@@ -22,6 +23,7 @@ export default function DeckPage() {
   }, []);
   
 
+  console.log(currentDeck);
   return (
     <div className="flex h-full flex-col items-center justify-between p-24 ">
       <h1>Savior of the universe</h1>
