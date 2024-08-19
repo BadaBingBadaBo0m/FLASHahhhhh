bde576c1d1b71d9a867d7c0d70d4d36bdbdc9527
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-18 20:07:22 -0700

    styled create deck modal and added validation

--- src/components/CreateDeck/index.js
+++ src/components/CreateDeck/index.js
@@ -8,11 +8,25 @@ import { db } from "@/app/firebase";
 const CreateDeckForm = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [nameError, setNameError] = useState();
+  const [descriptionError, setDescriptionError] = useState();
   const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("asl;dkfjasldkjf")
+
+    if (!name || name.length > 20 || description) {
+      if (!name || name.length > 20) setNameError("Name must be between 1 and 20 characters");
+
+
+      if (name && name.length <= 20) setNameError("");
+
+      if (description.length > 50) setDescriptionError("Description must be less than 50 characters");
+      console.log(description.length, "length")
+
+      return;
+    }
+
     if (!name) return;
 
     await addDoc(collection(db, "Decks"), {
@@ -26,8 +40,16 @@ const CreateDeckForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col justify-around items-center w-80 h-96">
-      <h1 className="text-black">Create Deck</h1>
+    <form onSubmit={handleSubmit} className="flex flex-col justify-around items-center w-80 p-12">
+      <h1 className="text-purple text-2xl font-bold mb-2">Create Deck</h1>
+
+      {nameError ? (
+        <>
+          <p className='text-red-700 text-lg text-center'>{nameError}</p>
+        </>
+      ) : (
+        <></>
+      )}
 
       <input
         placeholder="Name"
@@ -35,18 +57,26 @@ const CreateDeckForm = () => {
         required
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="text-black"
+        className="text-black border border-gray-400 p-2 outline-none w-60 mb-5"
       />
 
-      <input
+      {descriptionError ? (
+        <>
+          <p className='text-red-700 text-lg text-center'>{descriptionError}</p>
+        </>
+      ) : (
+        <></>
+      )}
+
+      <textarea
         placeholder="Description (Optional)"
         type="description"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
-        className="text-black"
+        className="text-black border border-gray-400 p-2 resize-none w-60 h-18 outline-none mb-5"
       />
 
-      <button onClick={handleSubmit} className="text-black bg-blue-500 rounded-full p-2">Create Deck</button>
+      <button onClick={handleSubmit} className="text-white bg-purple rounded-xl p-3 w-40">Create Deck</button>
     </form>
   )
 };
--- src/components/GenerateDeckFrom/index.js
+++ src/components/GenerateDeckFrom/index.js
@@ -25,7 +25,7 @@ const GenerateDeckForm = () => {
 
       if (count && count <= 10) setCountError("");
       return;
-    } 
+    }
 
     const deck = await runGemini(subject, count);
 
@@ -33,19 +33,19 @@ const GenerateDeckForm = () => {
       Name: subject,
       Cards: deck.Cards
     })
-  
+
     closeModal();
   }
 
   return (
-    <form className="flex flex-col items-center gap-7 p-16" onSubmit={handleSubmit}>
+    <form className="flex flex-col items-center gap-7 p-12" onSubmit={handleSubmit}>
       <h1 className="text-purple text-2xl font-bold">Generate new Deck</h1>
 
       {subjectError ? (
         <>
           <p className='text-red-700 text-lg'>{subjectError}</p>
         </>
-      ):  (
+      ) : (
         <></>
       )}
 
@@ -60,7 +60,7 @@ const GenerateDeckForm = () => {
         <>
           <p className='text-red-700 text-lg'>{countError}</p>
         </>
-      ):  (
+      ) : (
         <></>
       )}
 
